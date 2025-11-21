ed3f6d1babd7f649ee63cb4383975c8afab11f8c
Ceeminsola <ceemin19@gmail.com>
2025-11-22 00:37:23 +0100

    Fix

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,8 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
+const nextConfig = {
+  turbopack: false, 
+
   images: {
     formats: ["image/webp", "image/avif"],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
